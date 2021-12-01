ca70df1ff3f4ee2b5250726733d289e86f8d30ea
Black <vothanhlam1793@gmail.com>
2021-12-01 15:01:01 +0700

    fix

--- public/js/support.js
+++ public/js/support.js
@@ -2,6 +2,13 @@ var _support_vue = {
     methods: {
         numberWithCommas: function (x) {
             return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+        },
+        isMobile() {
+            if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+              return true
+            } else {
+              return false
+            }
         }
     }
 }
