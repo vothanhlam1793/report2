285ab4355332920b6d1dd65e6cc7660b21550466
CRETA Ops <ops@creta.vn>
2026-07-02 16:56:51 +0700

    fix: đơn giản hóa SessionStore - bỏ EventEmitter, xóa createIndex

--- app/config/session.store.js
+++ app/config/session.store.js
@@ -1,59 +1,38 @@
 const mongoose = require('mongoose')
-const EventEmitter = require('events')
 
-class SessionStore extends EventEmitter {
+module.exports = class SessionStore {
     constructor() {
-        super()
         this.model = mongoose.model('Session', new mongoose.Schema({
             _id: String,
             session: mongoose.Schema.Types.Mixed,
             expires: Date
         }, { timestamps: true }))
-        this.model.collection.createIndex({ expires: 1 }, { expireAfterSeconds: 0 }).catch(() => {})
     }
 
     get(sid, callback) {
         this.model.findById(sid).then(doc => {
-            if (!doc || (doc.expires && doc.expires < new Date())) {
-                return callback(null, null)
-            }
+            if (!doc) return callback(null, null)
             callback(null, doc.session)
-        }).catch(() => callback(null, null))
+        }).catch(err => callback(err))
     }
 
     set(sid, session, callback) {
-        const expires = session.cookie && session.cookie.maxAge
+        const expires = session.cookie?.maxAge
             ? new Date(Date.now() + session.cookie.maxAge)
             : new Date(Date.now() + 7 * 24 * 60 * 60 * 1000)
         this.model.findByIdAndUpdate(sid, {
             session: session,
             expires: expires
-        }, { upsert: true, new: true }).then(() => {
+        }, { upsert: true, new: true, setDefaultsOnInsert: true }).then(() => {
             callback()
         }).catch(callback)
     }
 
     destroy(sid, callback) {
-        this.model.findByIdAndDelete(sid).then(() => {
-            callback()
-        }).catch(callback)
-    }
-
-    createSession(req, sess) {
-        return sess
+        this.model.findByIdAndDelete(sid).then(() => callback()).catch(callback)
     }
 
     touch(sid, session, callback) {
-        if (session && session.cookie && session.cookie.maxAge) {
-            this.model.findByIdAndUpdate(sid, {
-                expires: new Date(Date.now() + session.cookie.maxAge)
-            }).then(() => {
-                callback()
-            }).catch(callback)
-        } else {
-            callback()
-        }
+        callback()
     }
 }
-
-module.exports = SessionStore
